9e8d6ed79e2a3faf3e682f6ab6d025ac08d184ea
Tati Fernandes <tatifernandes026@gmail.com>
2022-01-06 11:22:14 +0000

    first commit

---
+++ src/components/Header/header.js
@@ -0,0 +1,23 @@
+import './header.css'
+
+
+export const Headers = () =>{
+    return(
+        
+
+        <header className="header">
+            <div className="nav-bar">
+                <span>Search Cocktails</span>
+                <link href='/About'>
+                    <span>About</span>
+                </link>
+                <span>My Bar</span>
+            </div>
+
+
+
+
+
+        </header>
+    )
+}
