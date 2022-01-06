d8e6f639ec7b45eac87562afdce849522f508a70
Tati Fernandes <tatifernandes026@gmail.com>
2022-01-06 12:59:02 +0000

    react fixed

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -8,9 +8,9 @@ export const Headers = () =>{
         <header className="header">
             <div className="nav-bar">
                 <span>Search Cocktails</span>
-                <link href='/About'>
+              
                     <span>About</span>
-                </link>
+               
                 <span>My Bar</span>
             </div>
 
