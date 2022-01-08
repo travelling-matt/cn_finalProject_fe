826a1b8b955a463ba9996ff7fa543e91a5f98fef
DavidALevy <david.levy.levy@btinternet.com>
2022-01-08 23:10:15 +0000

    typo

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,9 +1,9 @@
-import './Header.css'
+import './header.css'
 
 // This component is the Header at the top of the main page
 // Contains a HeaderButton component for each of the Pages available on the website
 
-export const Header = () =>{
+export const Headers = () =>{
     return(
         <header className="header">
             <div className="nav-bar">
